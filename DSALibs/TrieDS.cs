6037d8f3a20652eac3aee8c28af40474ef646ef7
subhamoy-burman <subhamoy.2.burman@gmail.com>
2025-03-12 10:13:22 +0530

    Suffix Tree DS

--- DSALibs/TrieDS.cs
+++ DSALibs/TrieDS.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DSALibs
 {
@@ -24,21 +25,78 @@ namespace DSALibs
             currentNode.IsLeaf = true;
         }
 
-        public bool Search(string word) {
+        public bool Search(string word)
+        {
 
             var currentNode = RootNode;
 
-            foreach (var character in word) {
+            foreach (var character in word)
+            {
 
-                if (!currentNode.Childrens.ContainsKey(character)) { 
-                
+                if (!currentNode.Childrens.ContainsKey(character))
+                {
+
                     return false;
                 }
                 currentNode = currentNode.Childrens[character];
             }
 
             return currentNode.IsLeaf;
-        
+
+        }
+
+        public TrieNode BuildSuffix(string str)
+        {
+            TrieNode suffixTreeNode = new TrieNode();
+            for (int i = 0; i < str.Length; i++)
+            {
+                BuildSuffixTree(suffixTreeNode, str.Substring(i));
+            }
+            return suffixTreeNode;
+        }
+
+        private void BuildSuffixTree(TrieNode rootNode, string subStr)
+        {
+            var node = rootNode;
+            foreach (var item in subStr)
+            {
+                if (!node.Childrens.ContainsKey(item))
+                {
+                    node.Childrens.Add(item, new TrieNode());
+                }
+                node = node.Childrens[item];
+            }
+            node.IsLeaf = true;
+        }
+    }
+
+    public class SuffixTrie
+    {
+        public TrieNode RootNode { get; set; }
+
+        public SuffixTrie()
+        {
+            RootNode = new TrieNode();
+        }
+
+        public void BuildSuffix(string str)
+        {
+            for(int i = 0; i < str.Length; i++)
+            {
+                BuildSuffixTree(str.Substring(i));
+            }
+        }
+
+        private void BuildSuffixTree(string subStr)
+        {
+            foreach (var item in subStr)
+            {
+                if (!node.Childrens.ContainsKey(item))
+                {
+                    node.Childrens.Add(item, new TrieNode());
+                }
+                node = node.Childrens[item];
+            }
         }
     }
 

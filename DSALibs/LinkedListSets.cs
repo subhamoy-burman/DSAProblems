0afa7654ba4d69dc31c27df7df7e55aaeebebc3a
subhamoy-burman <subhamoy.2.burman@gmail.com>
2025-03-23 12:45:21 +0530

    Rotate LinkedList

--- DSALibs/LinkedListSets.cs
+++ DSALibs/LinkedListSets.cs
@@ -36,6 +36,47 @@ namespace DSALibs
 
     public class SingleLinkedList
     {
+        public SingleDLNode RotateLinkedList(int k)
+        {
+            if (Head == null || k == 0) return Head;
+            var currentNode = Head;
+            int length = 0;
+
+
+
+            while (currentNode != null)
+            {
+                currentNode = currentNode.Next;
+                length++;
+            }
+
+            if (length == 1 || k%length == 0) return Head;
+
+            SingleDLNode tempNode = null;
+            SingleDLNode currentOpNode = Head;
+            int traverseLength = length - (k % length);
+
+            while (traverseLength > 0)
+            {
+                tempNode = currentOpNode;
+                currentOpNode = currentOpNode.Next;
+                traverseLength--;
+            }
+
+            tempNode.Next = null;
+
+            var newHead = currentOpNode;
+
+            while (currentOpNode.Next != null)
+            {
+                currentOpNode = currentOpNode.Next;
+            }
+
+            currentOpNode.Next = Head;
+            Head = newHead;
+            return Head;
+
+        }
         public SingleDLNode? Head { get; set; }
 
         public SingleDLNode FindStartOfLoop()
@@ -175,26 +216,26 @@ namespace DSALibs
             return head;
 
         }
+
        
-    }
 
-    /*
-            Write a DoublyLinkedList class that has a head and a tail, both of which point to either a linked list Node or None / null. The class should support:
-            Setting the head and tail of the linked list.
-            Inserting nodes before and after other nodes as well as at given positions (the position of the head node is 1).
-            Removing given nodes and removing nodes with given values.
-            Searching for nodes with given values.
-            Note that the 
-            setHead, setTail, insertBefore, insertAfter, insertAtPosition, and remove methods all take in actual Nodes as input parameters not integers 
-            (except for insertAtPosition, which also takes in an integer representing the position); 
-            this means that you don't need to create any new Nodes in these methods. 
-            The input nodes can be either stand-alone nodes or nodes that are already in the linked list. 
-            If they're nodes that are already in the linked list, the methods will effectively be moving the nodes within the linked list. 
-            You won't be told if the input nodes are already in the linked list, so your code will have to defensively handle this scenario.
-         */
+        /*
+                Write a DoublyLinkedList class that has a head and a tail, both of which point to either a linked list Node or None / null. The class should support:
+                Setting the head and tail of the linked list.
+                Inserting nodes before and after other nodes as well as at given positions (the position of the head node is 1).
+                Removing given nodes and removing nodes with given values.
+                Searching for nodes with given values.
+                Note that the 
+                setHead, setTail, insertBefore, insertAfter, insertAtPosition, and remove methods all take in actual Nodes as input parameters not integers 
+                (except for insertAtPosition, which also takes in an integer representing the position); 
+                this means that you don't need to create any new Nodes in these methods. 
+                The input nodes can be either stand-alone nodes or nodes that are already in the linked list. 
+                If they're nodes that are already in the linked list, the methods will effectively be moving the nodes within the linked list. 
+                You won't be told if the input nodes are already in the linked list, so your code will have to defensively handle this scenario.
+             */
 
 
-    public class DoubleLinkedList
+        public class DoubleLinkedList
     {
         public DLNode? DLHead { get; set; }
 

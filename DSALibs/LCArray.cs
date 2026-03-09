2166c69d7a5c00a4d310235f4fc6a84015fbbdcb
subhamoy-burman <subhamoy.2.burman@gmail.com>
2026-03-09 20:21:04 +0530

    Merge Intervals

--- DSALibs/LCArray.cs
+++ DSALibs/LCArray.cs
@@ -1,34 +1,56 @@
 public class LCArray
 {
-        public void SortColors(int[] nums) {
-            int leftBoundary = 0;
-            int rightBoundary = nums.Length -1;
+    public void SortColors(int[] nums)
+    {
+        int leftBoundary = 0;
+        int rightBoundary = nums.Length - 1;
 
-            int i=0;
+        int i = 0;
 
-            while(i<nums.Length && i<=rightBoundary)
+        while (i < nums.Length && i <= rightBoundary)
+        {
+            if (nums[i] == 2)
             {
-                if(nums[i] == 2)
-                {
-                    var temp = nums[i];
-                    nums[i] = nums[rightBoundary];
-                    nums[rightBoundary] = temp;
-                    rightBoundary--;
-                }
-                else if(nums[i] == 0)
-                {
-                    var temp = nums[i];
-                    nums[i] = nums[leftBoundary];
-                    nums[leftBoundary] = temp;
-                    leftBoundary++;
-                    i++;
-                }
-                else
-                {
-                    i++;
-                }
+                var temp = nums[i];
+                nums[i] = nums[rightBoundary];
+                nums[rightBoundary] = temp;
+                rightBoundary--;
             }
-        
+            else if (nums[i] == 0)
+            {
+                var temp = nums[i];
+                nums[i] = nums[leftBoundary];
+                nums[leftBoundary] = temp;
+                leftBoundary++;
+                i++;
+            }
+            else
+            {
+                i++;
+            }
+        }
     }
-   
+
+    public List<Tuple<int, int>> MergeIntervals(List<Tuple<int, int>> intervals)
+    {
+        var result = new List<Tuple<int, int>>();
+        intervals.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+
+        result.Add(intervals[0]);
+
+        for (int i = 1; i < intervals.Count; i++)
+        {
+            if (intervals[i].Item1 <= result[result.Count - 1].Item2)
+            {
+                var temp = result[result.Count - 1].Item1;
+                result[result.Count - 1] = new Tuple<int, int>(temp, Math.Max(result[result.Count - 1].Item2, intervals[i].Item2));
+            }
+            else
+            {
+                result.Add(intervals[i]);
+            }
+        }
+        return result;
+    }
+
 }
